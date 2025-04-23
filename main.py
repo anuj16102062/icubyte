8645da1d193c799ae147ac867c1db20cd9db8477
anujf2p <anuj.pandey@farmtoplate.io>
2025-04-23 18:45:11 +0530

    test: handle newlines as delimiter

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ import re
 def add(numbers: str) -> int:
     if not numbers:
         return 0
+    delimiter = ",|\n"
+    custom_delimiter_match = re.match(r"^//(.+)\n(.*)", numbers)
+
+    if custom_delimiter_match:
+        delimiter = re.escape(custom_delimiter_match.group(1))
+        numbers = custom_delimiter_match.group(2)
+
+    split_numbers = re.split(delimiter, numbers)
+    num_list = [int(num) for num in split_numbers if num]
+    return sum(num_list)
--- test.py
+++ test.py
@@ -3,3 +3,17 @@ from main import add
 
 def test_empty_string_returns_zero():
     assert add("") == 0
+def test_single_number_returns_value():
+    assert add("1") == 1
+
+def test_two_numbers_comma_separated():
+    assert add("1,5") == 6
+
+def test_multiple_numbers_comma_separated():
+    assert add("1,2,3,4,5") == 15
+
+def test_numbers_separated_by_newlines():
+    assert add("1\n2,3") == 6
+
+def test_custom_delimiter():
+    assert add("//;\n1;2") == 3
